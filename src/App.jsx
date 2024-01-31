29ffd2ff916fbc21088658a6aa386ad2a0eeb6bb
bex9119 <60200396+bex9119@users.noreply.github.com>
2024-01-31 12:03:56 +0000

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import './App.css'
-import ProjectBody from './Components/ProjectBody';
-import Footer from './Components/Footer';
 import TopBar from './Components/TopBar';
-import { Col, Container, Row } from 'react-bootstrap';
-import background from './assets/bg3.png'
 import Home from './Components/Header';
 function App() {
   
   return (
 
       <div>
-
             <TopBar />
-
             <Home />
-
-
       </div>
 
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,63 +2,10 @@ import { Button, Col, Container, Row } from "react-bootstrap"
 
 const Home = () => {
     return (
-      //   <Container fluid>
       <>
         <h1 className="josefin-sans-h1">Rebecca Wallis</h1>
-
         <h2 className="josefin-sans-h2">Junior Software Developer</h2>
-
-        {/* <Row style={{ paddingBottom: "3rem" }}> */}
         <Button variant="dark" style={{padding: '1rem', paddingInline: '3rem'}}>Projects</Button>
-        {/* </Row> */}
-        {/* <Row style={{ paddingTop: "13rem" }}>
-          <Col>
-            <img
-              src="/JavaScriptLogo.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Col>
-          <Col>
-            <img
-              src="/TypeScriptLogo.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Col>
-          <Col>
-            <img
-              src="/reactLogo.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Col>
-          <Col>
-            <img
-              src="/NodeJs.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Col>
-          <Col>
-            <img
-              src="/ViteLogo.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-          </Col> */}
-        {/* </Row> */}
-        {/* </Container> */}
       </>
     );
 }
